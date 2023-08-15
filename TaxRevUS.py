1690d6f8c7c1431c92923fb0153e9fc070369369
AntonioMA581 <142312422+AntonioMA581@users.noreply.github.com>
2023-08-15 08:21:50 -0300

    Update TaxRevUS.py

--- TaxRevUS.py
+++ TaxRevUS.py
@@ -10,27 +10,9 @@ import matplotlib.pyplot as plt
 from statsmodels.tsa.filters.hp_filter import hpfilter
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[17]:
-
-
 df = pd.read_excel('Tax_Revenue_US.xlsx', sheet_name="MTS_RcptOutlyDfctSur_20180701_2")
 df = df.drop(['Current Month Gross Receipts Amount', 'Current Month Gross Outlay Amount'], axis=1)
 df
-
-
-# In[16]:
 
 
 FY18 = df[0:12]
@@ -40,18 +22,9 @@ FY21 = df[36:48]
 FY22 = df[48:60]
 FY23 = df[60:]
 
-
-# In[18]:
-
-
 series = df['Current Month Deficit Surplus Amount']
 
-
-# In[24]:
-
-
 ciclo_defsur, tend_defsur = hpfilter(series, lamb=14400)
-
 
 plt.style.use('seaborn')
 
@@ -59,16 +32,10 @@ fig, ax = plt.subplots(figsize=(12, 6))
 ax.plot( tend_defsur, label='Trend')
 ax.plot( series, label='Current Month Deficit Surplus Amount')
 
-
 ax.set_xlabel('Date')
 ax.set_ylabel('Deficit Surplus Amount')
 ax.set_title('Trend Deficit Surplus Amount')
 ax.legend(loc='upper left')
 plt.show()
-
-
-# In[ ]:
-
-
 
 
